a0c6dc268c496cfbf93cb6c0cdba23d68b4f59a0
AlyssonTrugilio <alyson.silvatrugilio121@gmail.com>
2024-08-05 15:47:27 -0300

    refactor: melhorado widget de código

--- lib/pages/code_otp_widget.dart
+++ lib/pages/code_otp_widget.dart
@@ -3,16 +3,18 @@ import 'package:flutter/services.dart';
 
 class CodeOtpWidget extends StatefulWidget {
   final void Function(String) onChanged;
+
   final int otpLength;
   final bool showCursor;
   final String? hintText;
-
+  final String? Function(String?)? validator;
   const CodeOtpWidget({
     super.key,
     required this.onChanged,
     required this.otpLength,
     this.showCursor = true,
     this.hintText,
+    this.validator,
   });
 
   @override
@@ -23,7 +25,7 @@ class CodeOtpWidgetState extends State<CodeOtpWidget>
     with SingleTickerProviderStateMixin {
   final TextEditingController _controller = TextEditingController();
   final FocusNode _focusNode = FocusNode();
-  late AnimationController _cursorController;
+  late final AnimationController _cursorController;
 
   @override
   void initState() {
@@ -51,111 +53,128 @@ class CodeOtpWidgetState extends State<CodeOtpWidget>
 
   @override
   Widget build(BuildContext context) {
+    const height = 48.0;
+    const width = 44.0;
+    const radius = 5.0;
+    const fontSize = 17.0;
+
     return GestureDetector(
-      onTap: () {
-        _focusNode.requestFocus();
-      },
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Positioned.fill(
-            child: Opacity(
-              opacity: 0,
-              child: TextFormField(
-                onChanged: widget.onChanged,
-                controller: _controller,
-                focusNode: _focusNode,
-                keyboardType: TextInputType.number,
-                inputFormatters: [
-                  FilteringTextInputFormatter.digitsOnly,
-                  LengthLimitingTextInputFormatter(widget.otpLength),
-                ],
-                decoration: const InputDecoration(
-                  border: InputBorder.none,
-                  fillColor: Colors.transparent,
-                  focusColor: Colors.transparent,
-                ),
-                cursorColor: Colors.transparent,
-              ),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: List.generate(widget.otpLength, (index) {
-              return Container(
-                width: 50,
-                height: 55,
-                alignment: Alignment.center,
-                margin: const EdgeInsets.symmetric(horizontal: 5),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: index == _controller.selection.baseOffset
-                        ? (_focusNode.hasFocus ? Colors.green : Colors.black)
-                        : Colors.black,
-                    width: 2,
-                  ),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      index < _controller.text.length
-                          ? _controller.text[index]
-                          : '',
-                      style: const TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+        onTap: _focusNode.requestFocus,
+        child: Column(
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Positioned.fill(
+                  child: Opacity(
+                    opacity: 0,
+                    child: TextFormField(
+                      onChanged: widget.onChanged,
+                      style: const TextStyle(fontSize: fontSize),
+                      controller: _controller,
+                      focusNode: _focusNode,
+                      validator: widget.validator,
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [
+                        FilteringTextInputFormatter.digitsOnly,
+                        LengthLimitingTextInputFormatter(widget.otpLength),
+                      ],
+                      decoration: InputDecoration(
+                        errorStyle: TextStyle(
+                          fontSize: 12,
+                          color: Theme.of(context).colorScheme.error,
+                        ),
+                        border: InputBorder.none,
+                        fillColor: Colors.transparent,
+                        focusColor: Colors.transparent,
                       ),
+                      cursorColor: Colors.transparent,
                     ),
-                    if (widget.showCursor &&
-                        _focusNode.hasFocus &&
-                        index == _controller.selection.baseOffset)
-                      AnimatedBuilder(
-                        animation: _cursorController,
-                        builder: (context, child) {
-                          return Opacity(
-                            opacity: _cursorController.value,
-                            child: Container(
-                              width: 2,
-                              height: 20,
-                              color: Colors.black,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: List.generate(widget.otpLength, (index) {
+                    return Container(
+                      width: width,
+                      height: height,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: index == _controller.selection.baseOffset
+                              ? (_focusNode.hasFocus
+                                  ? Theme.of(context).colorScheme.primary
+                                  : Colors.black)
+                              : Colors.black,
+                          width: index == _controller.selection.baseOffset &&
+                                  _focusNode.hasFocus
+                              ? 2
+                              : 1,
+                        ),
+                        borderRadius: BorderRadius.circular(radius),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            index < _controller.text.length
+                                ? _controller.text[index]
+                                : '',
+                            style: const TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                          if (widget.showCursor &&
+                              _focusNode.hasFocus &&
+                              index == _controller.selection.baseOffset)
+                            AnimatedBuilder(
+                              animation: _cursorController,
+                              builder: (context, child) {
+                                return Opacity(
+                                  opacity: _cursorController.value,
+                                  child: Container(
+                                    width: 2,
+                                    height: 20,
+                                    color: Colors.black,
+                                  ),
+                                );
+                              },
+                            ),
+                        ],
+                      ),
+                    );
+                  }),
+                ),
+                if (widget.hintText != null)
+                  Positioned(
+                    top: 0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(
+                        widget.otpLength,
+                        (index) {
+                          return Container(
+                            width: width,
+                            height: height,
+                            alignment: Alignment.center,
+                            margin: const EdgeInsets.symmetric(horizontal: 5),
+                            child: Text(
+                              index < _controller.text.length
+                                  ? ''
+                                  : widget.hintText!,
+                              style: const TextStyle(
+                                fontSize: 24,
+                                color: Colors.grey,
+                              ),
                             ),
                           );
                         },
                       ),
-                  ],
-                ),
-              );
-            }),
-          ),
-          if (widget.hintText != null)
-            Positioned(
-              top: 0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: List.generate(
-                  widget.otpLength,
-                  (index) {
-                    return Container(
-                      width: 50,
-                      height: 55,
-                      alignment: Alignment.center,
-                      margin: const EdgeInsets.symmetric(horizontal: 5),
-                      child: Text(
-                        index < _controller.text.length ? '' : widget.hintText!,
-                        style: const TextStyle(
-                          fontSize: 24,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
+                    ),
+                  ),
+              ],
             ),
-        ],
-      ),
-    );
+          ],
+        ));
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'package:otp_input/pages/pages.dart';
 import 'package:otp_input/pages/view_code_changed.dart';
+
+import 'pages.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -18,35 +19,40 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CodeOtpWidget(
-              showCursor: true,
-              // hintText: '2',
-              otpLength: 6,
-              onChanged: (value) {
-                setState(
-                  () {
-                    codeValue = value;
-                  },
-                );
-              },
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ViewCodeChanged(
-                      code: codeValue,
-                    ),
-                  ),
-                );
-              },
-              child: const Text('Visualizar código'),
-            )
-          ],
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 354),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CodeOtpWidget(
+                showCursor: true,
+                otpLength: 6,
+                onChanged: (value) {
+                  setState(
+                    () {
+                      codeValue = value;
+                    },
+                  );
+                },
+              ),
+              const SizedBox(height: 50),
+              TextButton(
+                onPressed: () {
+                  codeValue.isEmpty
+                      ? null
+                      : Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ViewCodeChanged(
+                              code: codeValue,
+                            ),
+                          ),
+                        );
+                },
+                child: const Text('Visualizar código'),
+              )
+            ],
+          ),
         ),
       ),
     );
